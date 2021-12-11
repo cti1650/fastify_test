062da14b07814bf85f2c4c92ff637e56f0a90813
cti1650 <cti1650@gmail.com>
2021-12-11 09:35:00 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # fastify_test
+
+## fastify
+
+### fastify 導入
+
+```bash
+yarn
+yarn setup
+```
+
+### サンプル
+
+#### 本番リンク
+
+[https://[id].deta.dev/](https://[id].deta.dev/)
+
+```node.js
+
+```
+
+### サーバー立ち上げ
+
+```
+yarn start
+```
+
+### Deta 登録
+
+[Deta Cloud](https://www.deta.sh/?ref=fastapi)
+
+### Deta CLI インストール
+
+```
+curl -fsSL https://get.deta.dev/cli.sh | sh
+```
+
+### Deta 環境変数定義
+
+```
+echo 'export PATH=~/.deta/bin:$PATH' >> ~/.bash_profile && source ~/.bash_profile
+```
+
+### Deta 動作確認
+
+```
+deta --help
+~/.deta/bin/deta --help
+```
+
+### Deta CLI Login (エラーになるため Chrome で実施要)
+
+```
+deta login
+~/.deta/bin/deta login
+```
+
+### Deta 新規連携
+
+```
+deta new
+~/.deta/bin/deta new
+```
+
+### Deta デプロイ
+
+```
+deta deploy
+~/.deta/bin/deta deploy
+```
+
+## 参考にしたサイト
+
+- [Deta にデプロイ - FastAPI](https://fastapi.tiangolo.com/ja/deployment/deta/)
+- [DETA](https://web.deta.sh/home/cti1650/default/micros)
+- [brew でインストールに失敗する時の対処メモ](https://zenn.dev/souq/articles/3c0591a50f39269793c9)
+- [Chrome ウェブストア - gitpod](https://chrome.google.com/webstore/search/gitpod?hl=ja)
+- [Node.js で環境変数を利用する方法](https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html-jp)
+- [Deploy your app or API on Deta Micros | Deta Docs](https://docs.deta.sh/docs/micros/deploy/#nodejs)
+- [Node.js + fastify + TypeScript + Swagger で REST サーバを構築する](https://zenn.dev/mokyn/articles/b5f727d30596fe)
+- [swmokyun/sample-fastify-typescript-swagger](https://github.com/swmokyun/sample-fastify-typescript-swagger)
---
+++ main.ts
@@ -0,0 +1,105 @@
+import { Static, Type } from "@sinclair/typebox"
+import fastify, { FastifyInstance } from "fastify"
+import fastifySwagger from "fastify-swagger"
+import { config } from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  config();
+}
+
+const PORT = process.env.PORT || 9000;
+
+const server: FastifyInstance = fastify({ logger: true })
+
+/**
+ * swagger
+ */
+server.register(fastifySwagger, {
+  routePrefix: "/docs",
+  swagger: {
+    info: {
+      title: "Test swagger",
+      description: "Testing the Fastify swagger API",
+      version: "0.1.0",
+    },
+  },
+  exposeRoute: true,
+})
+
+/**
+ * test_post
+ * https://www.fastify.io/docs/latest/TypeScript/#typebox
+ */
+const User = Type.Object({
+  name: Type.String(),
+  mail: Type.Optional(Type.String({ format: "email" })),
+})
+type UserType = Static<typeof User>
+
+server.post<{ Body: UserType; Reply: UserType }>(
+  "/test_post",
+  {
+    schema: {
+      body: User,
+      response: {
+        200: User,
+      },
+    },
+  },
+  (req, rep) => {
+    const { body: user } = req
+    rep.status(200).send(user)
+  }
+)
+
+/**
+ * test get, querystring
+ */
+const ErrorResponse = Type.Object({
+  msg: Type.String(),
+})
+type ErrorResponseType = Static<typeof ErrorResponse>
+
+server.get<{ Querystring: UserType; Reply: UserType | ErrorResponseType }>(
+  "/test_get",
+  {
+    schema: {
+      querystring: User,
+      response: {
+        200: User,
+        400: ErrorResponse,
+      },
+    },
+  },
+  (req, rep) => {
+    const { query: user } = req
+    if (user.name.length < 3) {
+      rep.status(400).send({ msg: "name is too short" })
+    } else {
+      rep.status(200).send(user)
+    }
+  }
+)
+
+server.get<{ Querystring: UserType; Reply: UserType | ErrorResponseType }>(
+  "/",
+  {
+    schema: {
+      querystring: User,
+      response: {
+        200: User,
+        400: ErrorResponse,
+      },
+    },
+  },
+  (req, rep) => {
+    const { query: user } = req
+    if (user.name.length < 3) {
+      rep.status(400).send({ msg: "name is too short" })
+    } else {
+      rep.status(200).send(user)
+    }
+  }
+)
+
+server.listen(PORT)
